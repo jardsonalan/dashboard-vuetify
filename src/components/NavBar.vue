6ef730ed83eabc58bca64b052af5b744f2f3a233
Jardson Alan <jardsonalan132@gmail.com>
2024-11-17 18:11:41 -0300

    update: menu dropdown do usuário e das notificações

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -25,17 +25,44 @@
         <v-app-bar-title>Meu App</v-app-bar-title>
 
         <template #append>
-            <v-btn icon class="mr-2">
-                <v-badge dot color="info">
-                    <v-icon icon="mdi-bell-outline"></v-icon>
-                </v-badge>
-            </v-btn>
-            <v-avatar>
-                <v-img
-                    src="https://images.pexels.com/photos/27333753/pexels-photo-27333753/free-photo-of-cidade-meio-urbano-moda-tendencia.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    cover
-                ></v-img>
-            </v-avatar>
+            <v-menu>
+                <template #activator="{ props }">
+                    <v-btn icon class="mr-2" v-bind="props">
+                        <v-badge dot color="info">
+                            <v-icon icon="mdi-bell-outline"></v-icon>
+                        </v-badge>
+                    </v-btn>
+                </template>
+                <v-card min-width="300px">
+                    <v-list :lines="false" density="compact">
+                        <v-list-item>
+                            <v-list-item-title>Notificações</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-card>
+            </v-menu>
+
+            <v-menu>
+                <template #activator="{ props }">
+                    <v-avatar v-bind="props">
+                        <v-img
+                            src="https://images.pexels.com/photos/27333753/pexels-photo-27333753/free-photo-of-cidade-meio-urbano-moda-tendencia.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                            cover
+                        ></v-img>
+                    </v-avatar>
+                </template>
+                <v-card min-width="200px">
+                    <v-list :lines="false" density="compact" nav>
+                        <v-list-item prepend-icon="mdi-account-outline">
+                            <v-list-item-title>Meu perfil</v-list-item-title>
+                        </v-list-item>
+                        <v-list-item prepend-icon="mdi-heart-outline">
+                            <v-list-item-title>Favoritos</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-card>
+            </v-menu>
+
         </template>
     </v-app-bar>
 </template>
