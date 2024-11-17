957733d9ca2706b27f87bc04553fdcf2dbbc26da
Jardson Alan <jardsonalan132@gmail.com>
2024-11-16 21:00:47 -0300

    update: sidebar e lista de menu aplicada

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,27 @@
 <template>
-    <v-app-bar flat>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-navigation-drawer v-model="isDrawerOpen">
+        <v-list>
+            <v-list-item prepend-icon="mdi-home">Home</v-list-item>
+            <v-list-item prepend-icon="mdi-account">Usuários</v-list-item>
+
+            <v-list-group value="Clientes">
+                <template v-slot:activator="{ props }">
+                    <v-list-item
+                        v-bind="props"
+                        prepend-icon="mdi-account-circle"
+                        title="Clientes"
+                    >
+                    </v-list-item>
+                </template>
+
+                <v-list-item prepend-icon="mdi-currency-usd">Vendas</v-list-item>
+                <v-list-item prepend-icon="mdi-chart-line">Relatório</v-list-item>
+            </v-list-group>
+        </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar flat class="border-b">
+        <v-app-bar-nav-icon @click="isDrawerOpen = !isDrawerOpen"></v-app-bar-nav-icon>
         <v-app-bar-title>Meu App</v-app-bar-title>
 
         <template #append>
@@ -20,5 +41,7 @@
 </template>
   
 <script setup lang="ts">
-    //
+import { ref } from 'vue';
+
+    const isDrawerOpen = ref(false)
 </script>  
